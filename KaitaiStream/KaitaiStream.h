968ec90bf85db4dc0d3acdcc7a17457270754179
Dmitry Marochko <mothlike@gmail.com>
2016-04-22 16:48:43 +0300

    Initial Commit

---
+++ KaitaiStream/KaitaiStream.h
@@ -0,0 +1,19 @@
+//
+//  KaitaiStream.h
+//  KaitaiStream
+//
+//  Created by Mothlike on 22.04.16.
+//  Copyright © 2016 Dmitry Marochko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KaitaiStream.
+FOUNDATION_EXPORT double KaitaiStreamVersionNumber;
+
+//! Project version string for KaitaiStream.
+FOUNDATION_EXPORT const unsigned char KaitaiStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaitaiStream/PublicHeader.h>
+
+
